bd1b7f49b5af3c009f286a414cf6a41a723be5b9
dpaulos6 <itzframepvp@outlook.com>
2025-01-14 17:49:39 +0000

    chore(release): 2.2.0

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -4,16 +4,38 @@ import { write } from './log'
 
 const configFilePath = path.join(__dirname, '..', './config.json')
 
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+/**
+ * Reads the configuration file. If the file does not exist, it creates a default configuration file.
+ * @returns {Record<string, any>} The configuration object.
+ */
 export const getConfig = (): Record<string, any> => {
   if (fs.existsSync(configFilePath)) {
-    const config = fs.readFileSync(configFilePath, 'utf-8')
-    return JSON.parse(config)
+    try {
+      const config = fs.readFileSync(configFilePath, 'utf-8')
+      return JSON.parse(config)
+    } catch (error) {
+      write({
+        message: `Error reading config file: ${error instanceof Error ? error.message : String(error)}`,
+        variant: 'error'
+      })
+    }
+  } else {
+    write({
+      message: 'Config file does not exist. Creating default config file.',
+      variant: 'warning'
+    })
+    const defaultConfig = createDefaultConfig()
+    saveConfig(defaultConfig)
+    return defaultConfig
   }
   return {}
 }
 
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+/**
+ * Saves the configuration object to the configuration file.
+ * @param {Record<string, any>} config - The configuration object to save.
+ * @returns {void}
+ */
 export const saveConfig = (config: Record<string, any>): void => {
   try {
     const directory = path.dirname(configFilePath)
@@ -24,8 +46,31 @@ export const saveConfig = (config: Record<string, any>): void => {
     fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2))
   } catch (error) {
     write({
-      message: `Error: ${error instanceof Error ? error.message : String(error)}`,
+      message: `Error saving config file: ${error instanceof Error ? error.message : String(error)}`,
       variant: 'error'
     })
+  }
+}
+
+/**
+ * Creates a default configuration object.
+ * @returns {Record<string, any>} The default configuration object.
+ */
+const createDefaultConfig = (): Record<string, any> => {
+  return {
+    publishConfig: {
+      isPackage: true,
+      publishTo: 'npm',
+      shouldPush: true,
+      shouldPublish: true,
+      commands: {
+        npm: {
+          publish: 'npm publish'
+        },
+        jsr: {
+          publish: 'jsr publish --username= --password='
+        }
+      }
+    }
   }
 }
--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 import chalk from 'chalk'
 
 type Variant = 'info' | 'success' | 'warning' | 'error'
@@ -14,5 +16,22 @@ export const write = ({
   }
   const icon = variantIcons[variant]
 
-  process.stdout.write(`${icon} ${chalk.white(message)}`)
+  process.stdout.write(`${icon} ${chalk.white(message)}\n`)
+}
+
+const logFilePath = path.join(__dirname, '..', './log.txt')
+
+/**
+ * Writes a log message to the log file.
+ * @param {Object} log - The log object containing the message and variant.
+ * @param {string} log.message - The log message.
+ * @param {string} log.variant - The log variant (info, success, warning, error).
+ * @returns {void}
+ */
+export const log = ({
+  message,
+  variant
+}: { message: string; variant: string }): void => {
+  const logMessage = `[${new Date().toISOString()}] [${variant.toUpperCase().slice(0, 4)}] ${message}\n`
+  fs.appendFileSync(logFilePath, logMessage)
 }
